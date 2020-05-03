f8cf73876d870a98c6e7ab5ce251c96afaa9acc7
juanjops <juanjose.pardo.s@gmail.com>
2020-05-03 19:34:25 +0200

    pagination improved

--- src/nodejs/index.js
+++ src/nodejs/index.js
@@ -1,9 +1,8 @@
 const C = require("./constants.js")
 const express = require("express")
 require("./db/mongoose")
-const linkedinRouter = require("./routers/linkedin")
-const getJobsId = require("./scraping/linkedin/jobs_id")
-const getJobContent = require("./scraping/linkedin/job_content")
+const linkedinRouter = require("./routers/linkedin.js")
+const linkedin_scraper = require("./scraping/linkedin.js")
 
 const app = express()
 const port = process.env.PORT || C.SERVER_PORT
@@ -22,13 +21,4 @@ const JOB_SEARCH_SPECS = {
     "time_range": "Past Week"
 }
 
-const main = async (jobs_search_specs) => {
-    try {
-        const jobs_id = await getJobsId(jobs_search_specs)
-        jobs_id.map(job_id => getJobContent(job_id))
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-main(JOB_SEARCH_SPECS)
+linkedin_scraper(JOB_SEARCH_SPECS)
