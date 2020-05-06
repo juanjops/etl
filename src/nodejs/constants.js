ba3a9c457b6a8d4de8683d4a00c4cfcd3f8daa77
juanjops <juanjose.pardo.s@gmail.com>
2020-05-06 20:57:06 +0200

    minor changes

--- src/nodejs/constants.js
+++ src/nodejs/constants.js
@@ -4,7 +4,7 @@ const USER = "juanjose.pardo.s@gmail.com"
 
 const PASSWORD = "malekith1990"
 
-const MONGODB_URL = 'mongodb+srv://jobs:f4Uo1b3ziIAhpPMf@cluster0-79fkx.mongodb.net/test?retryWrites=true&w=majority'
+const MONGODB_URL = 'mongodb+srv://jobs:f4Uo1b3ziIAhpPMf@cluster0-79fkx.mongodb.net/jobs?retryWrites=true&w=majority'
 
 module.exports = {
     SERVER_PORT,
