248a01e68bc4982d5b91d2bbfa98605a4b302136
juanjops <juanjose.pardo.s@gmail.com>
2020-05-15 00:55:18 +0200

    html page added

--- src/nodejs/public/client.js
+++ src/nodejs/public/client.js
@@ -1,11 +1,18 @@
 console.log('Client-side code running')
 
-const yesButton = document.getElementById('Yes-Button');
-yesButton.addEventListener('click', function(e) {
-  console.log('Yes button was clicked')
+const yesButton = document.getElementById('Yes-Button')
+const NoButton = document.getElementById('No-Button');
+const text = document.getElementById('text')
+
+const ar = [5,4,8]
+var ed = 0
+yesButton.addEventListener('click', (e) => {
+
+  text.textContent = ar[ed]
+  ed += 1
 })
 
-const NoButton = document.getElementById('No-Button');
-NoButton.addEventListener('click', function(e) {
+NoButton.addEventListener('click', (e) => {
+  e.preventDefault()
   console.log('No button was clicked')
 })
